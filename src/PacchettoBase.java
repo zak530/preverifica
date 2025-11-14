e5dce7ba7fcd5fe26ee6b9698ef5a598bfe8bb87
ELHAIKI.22132 <elhaiki.zakaria.studente@itispaleocapa.it>
2025-11-14 12:26:35 +0100

    metodo costruttore del pacchetto base

--- src/PacchettoBase.java
+++ src/PacchettoBase.java
@@ -1,2 +1,21 @@
+import java.util.Date;
+
 public class PacchettoBase {
+    private String descrizione;
+    private Date andata;
+    private Date ritorno;
+    private int costobase = 50;
+
+
+    public PacchettoBase(){
+        this.descrizione=descrizione;
+        this.andata=andata;
+        this.ritorno = ritorno;
+    }
+
+
+
+
+
+
 }
